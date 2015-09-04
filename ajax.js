08c6b2541dab2f50acde26b7111c7ea2b5b1b63d
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-09-04 12:45:31 -0400

    added get + post

--- ajax.js
+++ ajax.js
@@ -17,9 +17,11 @@ var $ = (function(){
     xhr.onload = function ( e ) {
       if ( xhr.readyState === 4 ) {
         if ( xhr.status === 200 ) {
-          console.log( defaultOpts.success() );
+          console.log( defaultOpts.success(xhr) );
+          // defaultOpts.success();
+          // console.log( this.responseText )
         } else {
-          console.error( defaultOpts.error() );
+          console.error( defaultOpts.error(xhr) );
         }
         console.log( defaultOpts.complete());
       }
@@ -30,10 +32,43 @@ var $ = (function(){
     xhr.send();
   }
 
+  var get = function(opts){
+    var defaultOpts = {
+      async: opts.async || false,
+      complete: opts.complete || function(){ console.log("Request Completed.")},
+      data : opts.data || {},
+      error: opts.error || function(){ console.log("Request Failure...")},
+      headers: opts.headers || { header: "Content-type", value: "application/x-www-form-urlencoded" },
+      method: opts.method || "GET",
+      success: opts.success || function(){ console.log("Request Successful!")},
+      url: opts.url || "http://reqr.es/users"
+    }
+
+    ajax(defaultOpts)
+  }
+
+  var post = function(opts){
+    var defaultOpts = {
+      async: opts.async || false,
+      complete: opts.complete || function(){ console.log("Request Completed.")},
+      data : opts.data || {},
+      error: opts.error || function(){ console.log("Request Failure...")},
+      headers: opts.headers || { header: "Content-type", value: "application/x-www-form-urlencoded" },
+      method: opts.method || "POST",
+      success: opts.success || function(){ console.log("Request Successful!")},
+      url: opts.url || "http://reqr.es/users",
+    }
+
+    ajax(defaultOpts)
+  }
+
   return {
     ajax: ajax,
+    get: get,
+    post: post
   };
 })();
 
-$.ajax({async: true, url: 'http://reqr.es/api/users?page=2'})
-$.ajax({async: true, url: 'http://reqr.es/api/users/23'})
+
+$.ajax({async: true, success: function(xhr){return xhr.responseText}, url: 'http://reqr.es/api/users?page=2'})
+$.ajax({async: true, url: 'http://reqr.es/api/users/23', error: function(xhr){return xhr.responseText}})
